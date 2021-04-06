f0e3c121f40a0f2f1f4d7e779ebc003688eda302
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-04-06 09:53:20 +0900

    Fix menu_view

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -17,4 +17,48 @@ class Menu < ApplicationRecord
   end
 
   validates :bar_code, format: { with: /\A[0-9]+\z/, message: 'は0から9の数字のみで入力してください' }
+
+  def self.menu_list(menu)
+    menu_ary = []
+    menu_hash = {}
+
+    value_1 = menu.amount / 3
+    value_2 = menu.amount / 2
+    value_3 = menu.amount
+    value_4 = menu.amount * 2
+    value_5 = menu.amount * 3
+
+    calorie_1 = menu.calorie / 3
+    calorie_2 = menu.calorie / 2
+    calorie_3 = menu.calorie
+    calorie_4 = menu.calorie * 2
+    calorie_5 = menu.calorie * 3
+
+    menu_hash[:value] = value_1
+    menu_hash[:calorie] = calorie_1
+    menu_ary << menu_hash
+    menu_hash = {}
+
+    menu_hash[:value] = value_2
+    menu_hash[:calorie] = calorie_2
+    menu_ary << menu_hash
+    menu_hash = {}
+
+    menu_hash[:value] = value_3
+    menu_hash[:calorie] = calorie_3
+    menu_ary << menu_hash
+    menu_hash = {}
+
+    menu_hash[:value] = value_4
+    menu_hash[:calorie] = calorie_4
+    menu_ary << menu_hash
+    menu_hash = {}
+
+    menu_hash[:value] = value_5
+    menu_hash[:calorie] = calorie_5
+    menu_ary << menu_hash
+    menu_hash = {}
+
+    return menu_ary
+  end
 end
