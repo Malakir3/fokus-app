445418a8946e86174e94f553ccc710efe8e1046c
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-04-14 22:00:47 +0900

    Install activerecord_import

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# users = []
+# 5.times do |i|
+#   users << User.new(firstname: "テスト", lastname: "太郎#{i+1}", nickname: "あだ名#{i+1}",
+#                     birthday: "1990-04-01", email: "test#{i+1}@com", encrypted_password: "$2a$12$bwDsWTtIDrymeWShpPyC5uedngcFB.fK2rkHzvc5utDnwOfZ1AGmm" )
+# end
+# User.import users
+
+menus = []
+5.times do |i|
+  menus << Menu.new(title: "食べ物#{i+1}", amount: (i + 100), unit: "g", calorie: (i + 300), bar_code: "000#{i+1}" )
+end
+Menu.import menus
