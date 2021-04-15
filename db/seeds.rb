b30aae0ae8abf4850fb22eef1afbd2f9eda8e75b
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-04-15 21:52:48 +0900

    Add csv files for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,19 @@
-# users = []
-# 5.times do |i|
-#   users << User.new(firstname: "テスト", lastname: "太郎#{i+1}", nickname: "あだ名#{i+1}",
-#                     birthday: "1990-04-01", email: "test#{i+1}@com", encrypted_password: "$2a$12$bwDsWTtIDrymeWShpPyC5uedngcFB.fK2rkHzvc5utDnwOfZ1AGmm" )
-# end
-# User.import users
-
-# 画像は添付されているはずだが、画像だけDBに保存されない
-# menus = []
-# 3.times do |i|
-#   menus << Menu.new(title: "食べ物#{i+1}", amount: (i + 100), unit: "g", calorie: (i + 300), bar_code: "000#{i+1}")
-#   5.times do
-#     menus[i].images.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
-#   end
-# end
-# Menu.import menus
-
-# CSV読み込みパターン
 require 'csv'
 
+CSV.foreach('db/csv/user.csv', headers: true) do |row|
+  User.create(
+    firstname: row[0],
+    lastname: row[1],
+    nickname: row[2],
+    birthday: row[3],
+    email: row[4],
+    password: row[5],
+    password_confirmation: row[6]
+  )
+end
+
 x = 1
-CSV.foreach('db/seed.csv', headers: true) do |row|
+CSV.foreach('db/csv/menu.csv', headers: true) do |row|
   menu = Menu.new(
     title: row[0],
     amount: row[1],
@@ -27,26 +21,34 @@ CSV.foreach('db/seed.csv', headers: true) do |row|
     calorie: row[3],
     bar_code: row[4]
   )
-
   5.times do |i|
-    menu.images.attach(io: File.open("public/images/#{x}/#{x}_#{i + 1}.jpg"), filename: "test_image_#{i + 1}.jpg")
+    menu.images.attach(io: File.open("public/images/#{x}/#{x}_#{i + 1}.jpg"), filename: "#{x}_#{i + 1}.jpg")
   end
   menu.save
-  
   x += 1
 end
 
+CSV.foreach('db/csv/standard.csv', headers: true) do |row|
+  Standard.create(
+    user_id: row[0],
+    menu_id: row[1],
+    large: row[2],
+    medium: row[3],
+    small: row[4]
+  )
+end
 
-# importは使用しないが、画像を保存可能なコード
-# 1.times do |i|
-#   menu = Menu.new(title: "食べ物#{i+1}", amount: (i + 100), unit: "g", calorie: (i + 300), bar_code: "000#{i+1}")
-#   5.times do
-#     menu.images.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
-#   end
-#   menu.save
-# end
+CSV.foreach('db/csv/intake.csv', headers: true) do |row|
+  Intake.create(
+    user_id: row[0],
+    menu_id: row[1],
+    date: row[2],
+    timing_id: row[3],
+    value_id: row[4]
+  )
+end
 
-# 画像だけDBに保存されない
+# （保留中）画像だけDBに保存されない
 # menus = []
 # 2.times do |i|
 #   menu = Menu.new(title: "食べ物#{i+1}", amount: (i + 100), unit: "g", calorie: (i + 300), bar_code: "000#{i+1}", images: [])
