2e8f0f205284b5e57139d0fdf6b17cfd6ad7a7ae
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-04-15 18:59:35 +0900

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,44 @@
 # end
 # Menu.import menus
 
-3.times do |i|
-  menu = Menu.new(title: "食べ物#{i+1}", amount: (i + 100), unit: "g", calorie: (i + 300), bar_code: "000#{i+1}")
-  5.times do
-    menu.images.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+# CSV読み込みパターン
+require 'csv'
+
+x = 1
+CSV.foreach('db/seed.csv', headers: true) do |row|
+  menu = Menu.new(
+    title: row[0],
+    amount: row[1],
+    unit: row[2],
+    calorie: row[3],
+    bar_code: row[4]
+  )
+
+  5.times do |i|
+    menu.images.attach(io: File.open("public/images/#{x}/#{x}_#{i + 1}.jpg"), filename: "test_image_#{i + 1}.jpg")
   end
   menu.save
+  
+  x += 1
 end
+
+
+# importは使用しないが、画像を保存可能なコード
+# 1.times do |i|
+#   menu = Menu.new(title: "食べ物#{i+1}", amount: (i + 100), unit: "g", calorie: (i + 300), bar_code: "000#{i+1}")
+#   5.times do
+#     menu.images.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+#   end
+#   menu.save
+# end
+
+# 画像だけDBに保存されない
+# menus = []
+# 2.times do |i|
+#   menu = Menu.new(title: "食べ物#{i+1}", amount: (i + 100), unit: "g", calorie: (i + 300), bar_code: "000#{i+1}", images: [])
+#   5.times do
+#     menu.images.attach(io: File.open(Rails.root.join('public/images/test_image.jpg')), filename: 'test_image.jpg')
+#   end
+#   menus << menu
+# end
+# Menu.import menus
